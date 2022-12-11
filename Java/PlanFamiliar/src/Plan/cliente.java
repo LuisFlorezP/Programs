4313f5844cf6a6f438ef81d48533bc8732867085
Luis Florez <braian1k3@gmail.com>
2022-12-10 20:29:56 -0500

    Agregar métodos para acceder a darle valores a cada atributo de la clase cliente como otra opción a parte de los constructores.

--- Java/PlanFamiliar/src/Plan/cliente.java
+++ Java/PlanFamiliar/src/Plan/cliente.java
@@ -10,6 +10,8 @@ public class cliente {
     private int menores;
     private String vehiculo;
     
+    public cliente() {}
+    
     public cliente(String nombre, String doc, int edad, char categoria, int mayores, int menores, String vehiculo) {
         this.nombre = nombre;
         this.doc = doc;
@@ -20,28 +22,56 @@ public class cliente {
         this.vehiculo = vehiculo;
     }
     
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+    
     public String getNombre() {
         return (nombre);
+    }
+    
+    public void setDoc(String doc) {
+        this.doc = doc;
     }
     
     public String getDoc() {
         return (doc);
     }
     
+    public void setEdad(int edad) {
+        this.edad = edad;
+    }
+    
     public int getEdad() {
         return (edad);
+    }
+    
+    public void setCategoria(char categoria) {
+        this.categoria = categoria;
     }
     
     public char getCategoria() {
         return (categoria);
     }
     
+    public void setMayores(int mayores) {
+        this.mayores = mayores;
+    }
+    
     public int getMayores() {
         return (mayores);
     }
     
+    public void setMenores(int menores) {
+        this.menores = menores;
+    }
+    
     public int getMenores() {
         return (menores);
+    }
+    
+    public void setVehiculo(String vehiculo) {
+        this.vehiculo = vehiculo;
     }
     
     public String getVehiculo() {
